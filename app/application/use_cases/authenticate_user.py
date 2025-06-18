01128e87e7ee7d5b4967b9be6154ac3f02d3fa0a
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2025-06-18 12:45:05 -0300

    fetch(jwt_service) implementação da secret_key

--- app/application/use_cases/authenticate_user.py
+++ app/application/use_cases/authenticate_user.py
@@ -1,22 +1,13 @@
 from app.domain.services.jwt_service import generate_token
 
-"""Esse caso de uso vai:
 
-1) Receber o e-mail e a senha como entrada=ok
-
-2) Buscar o usuário pelo e-mail bd
-
-3) Validar a senha com .verify() do Password()
-
-4) Retornar o usuário autenticado ou levantar erro
-"""
 class AuthenticateUserUseCase:
 
-    def __init__(self, user_repository):  # injeta o repositório de usuários, não o e-mail e senha direto
+    def __init__(self, user_repository): 
         self.user_repository = user_repository
 
     def execute(self, email: str, password: str):
-        # 1. Busca o usuário no banco
+        # 1. Busca o usuário no banco pelo email
         user = self.user_repository.search_by_email(email)
         if not user:
             raise ValueError("Email não encontrado")
--- app/domain/services/jwt_service.py
+++ app/domain/services/jwt_service.py
@@ -6,6 +6,9 @@ import os
 SECRET_KEY = os.environ.get("JWT_SECRET", "fallback_inseguro_somente_para_dev")
 
 def generate_token(user, expires_in_hours=1):
+    if SECRET_KEY == "fallback_inseguro_somente_para_dev":
+        raise ValueError("Não há Key secret")
+    
     payload = {
         "sub": str(user.id),
         "email": user.email.value,
